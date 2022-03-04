a8853b0b2a25a72874976c39fd621a31ec6a58b9
matias batista <matias.e.batista@hotmail.com git config --global user.email matias.e.batista@hotmail.com>
2022-03-04 19:05:22 -0300

    task

--- Argentina Programa- Angular/Tasklist/src/app/app.module.ts
+++ Argentina Programa- Angular/Tasklist/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { ButtonComponent } from './component/button/button.component';
+import { TasksComponent } from './component/tasks/tasks.component';
+import { TaskItemComponent } from './component/tasks-item/task-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { AddTaskComponent } from './component/add-task/add-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AddTaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
